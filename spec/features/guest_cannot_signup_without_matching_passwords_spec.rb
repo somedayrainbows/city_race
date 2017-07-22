41cabad06e18889d63e64d88534b0a359c4cf139
YOUR NAME <ebassity@gmail.com>
2017-07-21 23:50:40 -0600

    Edit test for matching password confirmation

--- spec/features/guest_cannot_signup_without_matching_passwords_spec.rb
+++ spec/features/guest_cannot_signup_without_matching_passwords_spec.rb
@@ -4,7 +4,9 @@ RSpec.feature "as an authenticated user, when I try to sign up" do
   scenario "I cannot sign up if password and confirmation do not match" do
     visit root_path
     save_and_open_page
-    click_on "Sign Up"
+    within(".navbar-collapse") do
+      click_on "Sign Up"
+    end
 
     expect(current_path).to eq(signup_path)
 
