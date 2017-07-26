a28d51c058a742dea5bbd4a897e2b4fed9e9894f
YOUR NAME <ebassity@gmail.com>
2017-07-25 22:09:00 -0600

    Add new hunt feature and styling

--- app/controllers/hunts_controller.rb
+++ app/controllers/hunts_controller.rb
@@ -10,19 +10,32 @@ class HuntsController < ApplicationController
   end
 
   def new
+    @hunt = current_user.hunts.new
 
   end
 
   def create
-
+    @hunt = current_user.hunts.new(hunt_params)
+    if @hunt.save
+      redirect_to hunts_path, notice: "Your hunt was created successfully."
+    else
+      render :new, notice: "Something went wrong. Please try again."
+    end
   end
 
   def edit
+    # require 'pry'; binding.pry
     @hunt = current_user.hunts.find_by(params[:id])
   end
 
   def destroy
     @hunt = current_user.hunts.find_by(params[:id])
+  end
+
+  private
+
+  def hunt_params
+    params.require(:hunt).permit(:name, :description)
   end
 
 end
