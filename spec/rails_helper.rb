6f1edf767bb4080997b5a930f846bba55cfea63b
Erin Bassity <ebassity@gmail.com>
2018-06-20 17:24:41 -0600

    update gems

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,6 @@
-require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
-# Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'capybara/rails'
@@ -18,33 +17,43 @@ end
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = false
   config.before(:suite) do
+    if config.use_transactional_fixtures?
+      raise(<<-MSG)
+        Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
+        (or set it to false) to prevent uncommitted transactions being used in
+        JavaScript-dependent specs.
+
+        During testing, the app-under-test that the browser driver connects to
+        uses a different database connection to the database connection used by
+        the spec. The app's database connection would not be able to access
+        uncommitted transaction data setup over the spec's database connection.
+      MSG
+    end
     DatabaseCleaner.clean_with(:truncation)
   end
-
 
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
 
+  config.before(:each, type: :feature) do
+    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
 
-  config.before(:each, :js => true) do
-    DatabaseCleaner.strategy = :truncation
+    if !driver_shares_db_connection_with_specs
+      DatabaseCleaner.strategy = :truncation
+    end
   end
-
-
   config.before(:each) do
     DatabaseCleaner.start
   end
-
-
-  config.after(:each) do
+  config.append_after(:each) do
     DatabaseCleaner.clean
   end
 
-  config.include FactoryGirl::Syntax::Methods
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 end
